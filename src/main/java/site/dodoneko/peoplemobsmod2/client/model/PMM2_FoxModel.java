78af5a05ce15a3854d7b14f77d7aab2119dc8885
どどど素人 <yfkf19961009@gmail.com>
2021-10-21 04:19:54 +0900

    きつねに仮のモーションを追加した

--- src/main/java/site/dodoneko/peoplemobsmod2/client/model/PMM2_FoxModel.java
+++ src/main/java/site/dodoneko/peoplemobsmod2/client/model/PMM2_FoxModel.java
@@ -31,30 +31,35 @@ public class PMM2_FoxModel<T extends FoxEntity> extends PMM2_BipedModel<T>
 //            this.bipedHead.rotateAngleX += -0.28F;
             this.bipedHead.rotateAngleY = MathHelper.sin(this.ageInTicks*0.28F) * 0.6F;
             this.bipedShippo.rotateAngleY = MathHelper.sin(this.ageInTicks*0.8F) * 0.32F;
-            this.bipedLeftEyelid.rotationPointZ = this.bipedRightEyelid.rotationPointZ = 0F;
-            this.bipedLeftEyelid.rotationPointY = this.bipedRightEyelid.rotationPointY = 0F;
             
             float temp1 = MathHelper.abs(MathHelper.sin(this.ageInTicks*0.28F)*MathHelper.sin(this.ageInTicks*0.28F*4));
-            this.bipedHead.rotateAngleX += (temp1>0.5f? temp1*2-1: 0)*10*PMM2_Math.Deg2Rad;
+            this.bipedHead.rotateAngleX += (temp1>0.5f? temp1*2-1: 0)*30F*PMM2_Math.Deg2Rad;
             
         }
-        if (this.entityIn.isSleeping()) {
+        else if (this.entityIn.isSleeping()) {
         	System.out.println("fox is sleeping");
+        	// 仮りのモーション
+            this.setSneakAnimations();
         }
-        if (this.entityIn.isSitting()) {
+        else if (this.entityIn.isSitting()) {
         	System.out.println("fox is sitting");
+        	// 仮りのモーション
+            this.setSneakAnimations();
         }
-        if (this.entityIn.func_213472_dX()) {
+        else if (this.entityIn.func_213472_dX()) {
         	System.out.println("fox is Fitting on snow");
         }
-        if (this.entityIn.func_213480_dY()) {
+        else if (this.entityIn.func_213480_dY()) {
         	System.out.println("fox is jumping");
         }
-        if (this.entityIn.func_213490_ee()) {
+        else if (this.entityIn.func_213490_ee()) {
         	System.out.println("fox is jumping ?");
         }
-        if (this.entityIn.func_213467_eg()) {
+        else if (this.entityIn.func_213467_eg()) {
         	System.out.println("fox is ready to jump");
+        	// 仮りのモーション
+            this.bipedBody.rotateAngleY = MathHelper.sin(this.ageInTicks*1F) * 0.42F;
+            this.setSneakAnimations();
         }
     }
 
@@ -64,6 +69,23 @@ public class PMM2_FoxModel<T extends FoxEntity> extends PMM2_BipedModel<T>
             // かがんで地面のにおいを追跡
             this.bipedLeftEyelid.rotationPointZ = this.bipedRightEyelid.rotationPointZ = 0F;
             this.bipedLeftEyelid.rotationPointY = this.bipedRightEyelid.rotationPointY = 0F;
+        }
+        else if (this.entityIn.isSleeping()) {
+        	// 仮りのモーション
+            this.bipedLeftEyelid.rotationPointZ = this.bipedRightEyelid.rotationPointZ = 0F;
+            this.bipedLeftEyelid.rotationPointY = this.bipedRightEyelid.rotationPointY = 0F;
+        }
+        else if (this.entityIn.isSitting()) {
+        }
+        else if (this.entityIn.func_213472_dX()) {
+        }
+        else if (this.entityIn.func_213480_dY()) {
+        }
+        else if (this.entityIn.func_213490_ee()) {
+        }
+        else if (this.entityIn.func_213467_eg()) {
+        	// 仮りのモーション
+            this.bipedBody.rotateAngleY = MathHelper.sin(this.ageInTicks*1F) * 0.42F;
         }
     }
 }
